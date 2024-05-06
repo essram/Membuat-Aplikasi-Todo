27758f3efeba04b5a9a58636e1ac84dd7e852b44
essram <esarama22@gmail.com>
2024-05-06 21:28:09 +0700

    Membuat template

--- src/index.php
+++ src/index.php
@@ -1,2 +1,32 @@
 <!-- Membuat Aplikasi Todo Membuat Template -->
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To do App</title>
+</head>
+
+<body>
+    <h1>
+        Todo App
+    </h1>
+
+    <form action="">
+        <label for="">Apa kegiatanmu Hari Ini?</label> <br>
+        <input type="text" name="todo">
+        <button type="submit">Simpan</button>
+    </form>
+
+    <ul>
+        <li>
+            <input type="checkbox" name="todo">
+            <label for="">Todo 1</label>
+            <a href="#">Hapus</a>
+        </li>
+    </ul>
+</body>
+
+</html>
